639ef69c04f5324e0d3c97c4c5defa7c9b6f6423
Patrick Pietens <hello@patrickpietens.com>
2017-11-12 23:56:12 +0100

    Added responsive videos

--- src/views/how-it-works.blade.php
+++ src/views/how-it-works.blade.php
@@ -1,81 +1,93 @@
 <section class='how-it-works how-it-works--add-your-lineup'>
-    <div class='how-it-works__content'>
-        <h1 class='how-it-works__title'>Add your lineup</h1>
-        <h2 class='how-it-works__subtitle'>Always up to date</h2>
+    <div>
+        <div class='how-it-works__info'>
+            <h1 class='how-it-works__title'>Add your lineup</h1>
+            <h2 class='how-it-works__subtitle'>Always up to date</h2>
 
-        <p class='how-it-works__description'>
-            Use our powerful CMS to update all your acts and speakers. Add multiple days and stages so you can make sure your visitors have all the information they need.
-        </p>
+            <p class='how-it-works__description'>
+                Use our powerful CMS to update all your acts and speakers. Add multiple days and stages so you can make sure your visitors have all the information they need.
+            </p>
 
-        <ol class='how-it-works__features'>
-            <li>Drag & drop interface</li>
-            <li>Multiple days & stages</li>
-            <li>Export your data</li>
-            <li>Invite your team members</li>
-        </ol>
-    </div>
+            <ol class='how-it-works__features'>
+                <li>Drag & drop interface</li>
+                <li>Multiple days & stages</li>
+                <li>Export your data</li>
+                <li>Invite your team members</li>
+            </ol>
+        </div>
 
-    <div class='how-it-works__media'>
-        <video id='addYourLineupVideo' class='how-it-works__video-player'></video>
+        <div class='how-it-works__media'>
+            <div class='how-it-works__video-wrapper'>
+                <video id='addYourLineupVideo' class='how-it-works__player'></video>
+            </div>
 
-        <ol class='how-it-works__keyframes'>
-            <li>Days</li>
-            <li>Stages</li>
-            <li>Acts</li>
-        </ol>
+            <ol class='how-it-works__keyframes'>
+                <li>Days</li>
+                <li>Stages</li>
+                <li>Acts</li>
+            </ol>
+        </div>
     </div>
 </section>
 
 <section class='how-it-works how-it-works--embed-your-timetable'>
-    <div class='how-it-works__content'>
-        <h1 class='how-it-works__title'>Embed your timetable</h1>
-        <h2 class='how-it-works__subtitle'>Up and running in no time</h2>
+    <div>
+        <div class='how-it-works__info'>
+            <h1 class='how-it-works__title'>Embed your timetable</h1>
+            <h2 class='how-it-works__subtitle'>Up and running in no time</h2>
 
-        <p class='how-it-works__description'>
-            Add a few lines of code to your website to add your timetable on both desktop and mobile. Visitors can save favorite acts by connecting their social media accounts.
-        </p>
+            <p class='how-it-works__description'>
+                Add a few lines of code to your website to add your timetable on both desktop and mobile. Visitors can save favorite acts by connecting their social media accounts.
+            </p>
 
-        <ol class='how-it-works__features'>
-            <li>Optimized for mobile</li>
-            <li>Fully customizable</li>
-            <li>Visitors can save favorite acts</li>
-        </ol>
-    </div>
+            <ol class='how-it-works__features'>
+                <li>Optimized for mobile</li>
+                <li>Fully customizable</li>
+                <li>Visitors can save favorite acts</li>
+            </ol>
+        </div>
 
-    <div class='how-it-works__media'>
-        <video id='embedYourTimetableVideo' class='how-it-works__video-player'></video>
+        <div class='how-it-works__media'>
+            <div class='how-it-works__video-wrapper'>
+                <video id='embedYourTimetableVideo' class='how-it-works__player'></video>
+            </div>
 
-        <ol class='how-it-works__keyframes'>
-            <li>Embed</li>
-            <li>Favorites</li>
-            <li>Mobile</li>
-        </ol>
+            <ol class='how-it-works__keyframes'>
+                <li>Embed</li>
+                <li>Favorites</li>
+                <li>Mobile</li>
+            </ol>
+        </div>
     </div>
 </section>
 
 <section class='how-it-works how-it-works--built-for-developers'>
-    <div class='how-it-works__content'>
-        <h1 class='how-it-works__title'>Built for developers</h1>
-        <h2 class='how-it-works__subtitle'>Customize your design</h2>
+    <div>
+        <div class='how-it-works__info'>
+            <h1 class='how-it-works__title'>Built for developers</h1>
+            <h2 class='how-it-works__subtitle'>Customize your design</h2>
 
-        <p class='how-it-works__description'>
-            Match the design of your timetable to your event’s branding with some simple CSS tweaks. You can change colors, typography or go all out and create your own design.
-        </p>
+            <p class='how-it-works__description'>
+                Match the design of your timetable to your event’s branding with some simple CSS tweaks. You can change colors, typography or go all out and create your own design.
+            </p>
 
-        <ol class='how-it-works__features'>
-            <li>Select different layouts</li>
-            <li>Fully customizable with CSS/JS</li>
-            <li>API access</li>
-            <li>In-depth code documentation</li>
-        </ol>
-    </div>
+            <ol class='how-it-works__features'>
+                <li>Select different layouts</li>
+                <li>Fully customizable with CSS/JS</li>
+                <li>API access</li>
+                <li>In-depth code documentation</li>
+            </ol>
+        </div>
 
-    <div class='how-it-works__media'>
-        <video id='builtForDevelopersVideo' class='how-it-works__video-player'></video>
+        <div class='how-it-works__media'>
+            <div class='how-it-works__video-wrapper'>
+                <video id='builtForDevelopersVideo' class='how-it-works__player'></video>
+            </div>
 
-        <ol class='how-it-works__keyframes'>
-            <li>Layouts</li>
-            <li>Customization</li>
-        </ol>
+            <ol class='how-it-works__keyframes'>
+                <li>Layouts</li>
+                <li>Customization</li>
+            </ol>
+        </div>
     </div>
 </section>
