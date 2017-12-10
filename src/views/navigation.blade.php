95c74cc0c11ea79de8d86a4f37d1263e9f9d5938
Patrick Pietens <hello@patrickpietens.com>
2017-12-11 00:00:40 +0100

    Added docs link to navigation

--- src/views/navigation.blade.php
+++ src/views/navigation.blade.php
@@ -10,6 +10,7 @@
         <li><a href='/showcase' class='navigation__link' data-link>@lang('brochure.navigation.showcase')</a></li>
         <li><a href='/pricing' class='navigation__link' data-link>@lang('brochure.navigation.pricing')</a></li>
         <li><a href='/contact' class='navigation__link' data-link>@lang('brochure.navigation.contact')</a></li>
+        <li><a href='https://blokks.co/docs' target='docs' class='navigation__link navigation__link--docs'>Docs</a></li>
     </ol>
 
     <div class='navigation__account'>
