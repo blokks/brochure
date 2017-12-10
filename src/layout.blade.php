1f5906342d43268344d6ed1d7c34a22af3f8377f
Patrick Pietens <hello@patrickpietens.com>
2017-12-10 23:51:38 +0100

    Added GA and social mediacards

--- src/layout.blade.php
+++ src/layout.blade.php
@@ -11,6 +11,32 @@
         <link href='{{ url('/brochure/dist/styles/screen.css') }}' rel='stylesheet'>
         <link href='{{ static_url('images/favicon.png') }}' rel='shortcut icon' type='image/x-icon'>
 
+        <!-- Standard -->
+        <meta name='description' content='@lang('brochure.social.description')'>
+        <meta name='keywords' content='@lang('brochure.social.keywords')'>
+        <meta name='author' content='BlokksHQ'>
+        <link rel="canonical" href="https://blokks.co">
+
+        <!-- Schema.org -->
+        <meta itemprop='name' content='Blokks'>
+        <meta itemprop='description' content='@lang('brochure.social.description')'>
+        <meta itemprop='url' content='https://blokks.co'>
+        <meta itemprop='keywords' content='@lang('brochure.social.keywords')'>
+        <meta itemprop='image' content='{{ url("/brochure/dist/images/og-image-new.jpg") }}'>
+        <meta itemprop='sourceOrganization' content='Blokks'>
+        <meta itemprop='inLanguage' content='en-US'>
+
+        <!-- OpenGraph -->
+        <meta property='og:type' content='website'>
+        <meta property='og:title' content='Blokks'>
+        <meta property='og:description' content='@lang('brochure.social.description')'>
+        <meta property='og:url' content='{{ url() }}'>
+        <meta property='og:image' content='{{ url("/brochure/dist/images/og-image-new.jpg") }}'/>
+
+        <!-- Twitter Card -->
+        <meta name='twitter:card' content='summary'>
+        <meta name='twitter:site' content='@BlokksHQ'>
+
         @stack('styles')
     </head>
 
@@ -23,5 +49,28 @@
 
         <script src='{{ url('/brochure/dist/scripts/vendor/SnapSVGAnimator.js') }}'></script>
         <script src='{{ url('/brochure/dist/scripts/index.js') }}'></script>
+
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:334270,hjsv:5};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+
+        <script>
+            (function(i,s,o,g,r,a,m){
+                i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })
+            (window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-64372736-5', 'auto');
+            ga('send', 'pageview');
+        </script>        
     </body>
 </html>
