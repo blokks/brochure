047a9d4448b5d66db0c46b3b20e3f6c2cfac3b97
Patrick Pietens <hello@patrickpietens.com>
2018-01-04 22:28:46 +0100

    Added signup email link to mobile version

--- src/views/call-to-action.blade.php
+++ src/views/call-to-action.blade.php
@@ -1,11 +1,25 @@
 <section class='call-to-action' data-view='contact' data-path='/contact'>
-    <div class='call-to-action__content'>
+    <div class='call-to-action__content call-to-action__content--mobile'>
         <h1 class='call-to-action__title'>
             @lang('brochure.call_to_action.title')
         </h1>
 
         <h2 class='call-to-action__subtitle'>
-            @lang('brochure.call_to_action.subtitle')
+            @lang('brochure.call_to_action.mobile.subtitle')
+        </h2>
+
+        <a href="mailto:reminder@blokks.co?subject=@lang('brochure.call_to_action.mobile.reminder_subject')&body=@lang('brochure.call_to_action.mobile.reminder_body')" class='call-to-action__action-button'>
+            @lang('brochure.call_to_action.signup')
+        </a>    
+    </div>
+
+    <div class='call-to-action__content call-to-action__content--desktop'>
+        <h1 class='call-to-action__title'>
+            @lang('brochure.call_to_action.title')
+        </h1>
+
+        <h2 class='call-to-action__subtitle'>
+            @lang('brochure.call_to_action.desktop.subtitle')
         </h2>
 
         <a href='{{ route('signup') }}' class='call-to-action__action-button'>
